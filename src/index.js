4671f35761c9f38f669de9a11edbeb4e6fed98c3
Lawrence Amoafo <rekenburg777@gmail.com>
2023-04-12 19:25:42 +0000

    Display web contents
    Added an eventlistener to both the form and the refresh button. This is enable the display of the user inputs from the API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import './style.css';
-import showScores from './recentScore.js';
-import addScore from './addYourScore.js';
+import recentScores from './module/recentScore.js';
+import addYourScore from './module/addYourScore.js';
+import addScore from './module/addScore.js';
+import showScores from './module/showScore.js';
+import displayScores from './module/displayScore.js';
 
-showScores();
-addScore();
+recentScores();
+addYourScore();
+
+const form = document.querySelector('form');
+const userName = document.querySelector('#name');
+const userScore = document.querySelector('#number');
+const Scores = document.querySelector('.scores');
+const refreshButton = document.querySelector('#refresh');
+
+const scoreContainer = () => {
+  Scores.innerHTML = '';
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (userName.value && userScore.value) {
+    addScore(userName.value.trim(), userScore.value.trim());
+    scoreContainer();
+    form.reset();
+    displayScores().then((obj) => showScores(obj, Scores));
+  }
+});
+
+refreshButton.addEventListener('click', () => {
+  scoreContainer();
+  displayScores().then((obj) => showScores(obj, Scores));
+});
